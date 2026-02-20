35a69d7f260a8cf78ebfd8d0d7e072c809c34d10
surasitohk7486 <surasit.dang@bumail.net>
2026-02-20 16:52:55 +0700

    WIP

--- Assets/Scripts/Exam/PlayerControllerExam05.cs
+++ Assets/Scripts/Exam/PlayerControllerExam05.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,25 +6,17 @@ public class PlayerControllerExam05 : MonoBehaviour
     public float speed;
     public float xRange = 10;
     public GameObject projectilePrefab;
-
-
     // Exam 05 ...
     public int maxBulletCount = 10;
     public float bulletRegenerateCooldown = 1f;
-    private int currentBullet;
-    private bool isReloading = false;
-    private float reloadTimer = 5f;
     // ...
-
     private float horizontalInput;
     private InputAction moveAction;
     private InputAction shootAction;
-
     private void Awake()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         shootAction = InputSystem.actions.FindAction("Shoot");
-        currentBullet = maxBulletCount;
     }
 
     // Update is called once per frame
@@ -42,28 +33,25 @@ public class PlayerControllerExam05 : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        if (shootAction.WasPressedThisFrame() && !isReloading)
-        {
-            if (currentBullet > 0)
-            {
-                Instantiate(projectilePrefab, transform.position, Quaternion.identity);
-                currentBullet--;
 
-                if (currentBullet <= 0 )
-                {
-                    isReloading = true;
-                    reloadTimer = bulletRegenerateCooldown;
-                }
-                if (isReloading)
-                {
-                    reloadTimer -= Time.deltaTime;
-                    if (reloadTimer <= 5f)
-                    {
-                        currentBullet = maxBulletCount;
-                        
-                    }
-                }
+        if (maxBulletCount > 0)
+        {
+            if (shootAction.triggered)
+            {
+                Instantiate(projectilePrefab, transform.position, transform.rotation);
+                maxBulletCount--;
+            }
+
+        }
+        else if (maxBulletCount == 0)
+        {
+            bulletRegenerateCooldown -= Time.deltaTime;
+            if (bulletRegenerateCooldown < 0)
+            {
+                maxBulletCount = 10;
+                bulletRegenerateCooldown = 5f;
             }
         }
+
     }
 }
